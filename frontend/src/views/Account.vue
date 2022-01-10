d6767e78e2662087b1f3162e4bee702153aa9fd5
Melinda Sth <sayasith.melinda.pro@gmail.com>
2022-01-10 04:06:01 +0100

    Style Account

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -8,72 +8,79 @@
           <font-awesome-icon icon="fa-regular fa-trash-alt" />
         </button>
       </h2>
-      <p>Nom :</p>
-      <div v-if="this.show.lastname == false">
-        <p class="account__content__list">{{ userAccount.lastname }}</p>
+      <div class="account__container__element">
+        <p>Nom :</p>
+        <div v-if="this.show.lastname == false">
+          <p class="account__content__list">{{ userAccount.lastname }}</p>
+        </div>
+        <div v-if="this.show.lastname == true" class="account__content__input">
+          <input v-model="modifyInput.lastname" placeholder="Nouveau nom" />
+          <button
+            class="button"
+            type="button"
+            v-on:click="UpdateUser({ lastname: modifyInput.lastname })"
+          > <font-awesome-icon icon="fa-regular fa-paper-plane" />
+          </button>
+        </div>
+        <button v-on:click="changeName()" class="button">Modifier</button>
       </div>
-      <div v-if="this.show.lastname == true" class="account__content__input">
-        <input v-model="modifyInput.lastname" placeholder="Nouveau nom" />
-        <button
-          class="button"
-          type="button"
-          v-on:click="UpdateUser({ lastname: modifyInput.lastname })"
+      <div class="account__container__element">
+        <p>Prénom :</p>
+        <div
+          v-if="this.show.firstname == false"
+          class="account__content__input"
         >
-          Modifier
-        </button>
+          <p class="account__content__list">{{ userAccount.firstname }}</p>
+        </div>
+        <div v-if="this.show.firstname == true" class="account__content__input">
+          <input v-model="modifyInput.firstname" placeholder="Nouveau prénom" />
+          <button
+            class="button"
+            type="button"
+            v-on:click="UpdateUser({ firstname: modifyInput.firstname })"
+          ><font-awesome-icon icon="fa-regular fa-paper-plane" />
+          </button>
+        </div>
+        <button v-on:click="changeFirstname()" class="button">Modifier</button>
       </div>
-      <button v-on:click="changeName()" class="button">Modifier</button>
-      <p>Prénom :</p>
-      <div v-if="this.show.firstname == false" class="account__content__input">
-        <p class="account__content__list">{{ userAccount.firstname }}</p>
+      <div class="account__container__element">
+        <p>E-mail :</p>
+        <div v-if="this.show.email == false" class="account__content__input">
+          <p class="account__content__list">{{ userAccount.email }}</p>
+        </div>
+        <div v-if="this.show.email == true" class="account__content__input">
+          <input v-model="modifyInput.email" placeholder="Nouvel e-mail" />
+          <button
+            class="button"
+            type="button"
+            v-on:click="UpdateUser({ email: modifyInput.email })"
+          ><font-awesome-icon icon="fa-regular fa-paper-plane" />
+          </button>
+        </div>
+        <button v-on:click="changeEmail()" class="button">Modifier</button>
       </div>
-      <div v-if="this.show.firstname == true" class="account__content__input">
-        <input v-model="modifyInput.firstname" placeholder="Nouveau prénom" />
-        <button
-          class="button"
-          type="button"
-          v-on:click="UpdateUser({ firstname: modifyInput.firstname })"
-        >
-          Modifier
-        </button>
+      <div class="account__container__element">
+        <p>Poste :</p>
+        <div v-if="this.show.poste == false" class="account__content__input">
+          <p class="account__content__list">{{ userAccount.jobtitle }}</p>
+        </div>
+        <div v-if="this.show.poste == true" class="account__content__input">
+          <select name="jobtile" id="jobtitle" v-model="modifyInput.jobtitle">
+            <option value="IT">IT</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Direction">Direction</option>
+            <option value="RH">RH</option>
+            <option value="Finance">Finance</option>
+          </select>
+          <button
+            class="button"
+            type="button"
+            v-on:click="UpdateUser({ jobtitle: modifyInput.jobtitle })"
+          >
+          </button>
+        </div>
+        <button v-on:click="changeJobtitle()" class="button">Modifier</button>
       </div>
-      <button v-on:click="changeFirstname()" class="button">Modifier</button>
-      <p>E-mail :</p>
-      <div v-if="this.show.email == false" class="account__content__input">
-        <p class="account__content__list">{{ userAccount.email }}</p>
-      </div>
-      <div v-if="this.show.email == true" class="account__content__input">
-        <input v-model="modifyInput.email" placeholder="Nouvel e-mail" />
-        <button
-          class="button"
-          type="button"
-          v-on:click="UpdateUser({ email: modifyInput.email })"
-        >
-          Modifier
-        </button>
-      </div>
-      <button v-on:click="changeEmail()" class="button">Modifier</button>
-      <p>Poste :</p>
-      <div v-if="this.show.poste == false" class="account__content__input">
-        <p class="account__content__list">{{ userAccount.jobtitle }}</p>
-      </div>
-      <div v-if="this.show.poste == true" class="account__content__input">
-        <select name="jobtile" id="jobtitle" v-model="modifyInput.jobtitle">
-          <option value="IT">IT</option>
-          <option value="Marketing">Marketing</option>
-          <option value="Direction">Direction</option>
-          <option value="RH">RH</option>
-          <option value="Finance">Finance</option>
-        </select>
-        <button
-          class="button"
-          type="button"
-          v-on:click="UpdateUser({ jobtitle: modifyInput.jobtitle })"
-        >
-          Modifier
-        </button>
-      </div>
-      <button v-on:click="changeJobtitle()" class="button">Modifier</button>
     </div>
   </section>
 </template>
@@ -237,11 +244,18 @@ export default {
   justify-content: space-around;
 }
 .account__content__list {
-  margin: 20px;
+  margin: 10px;
 }
 .account__content__input {
   display: flex;
   margin-bottom: 10px;
+}
+.account__container__element {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
 }
 .button {
   background-color: white;
